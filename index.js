9e1ec13f2ee9a050abfa9d3cca0c7992ccfcf877
Mohamed Ibrahim <109217422+memo-ibrahim-alean@users.noreply.github.com>
2022-11-21 14:11:06 +0200

    Add modal messages 3

--- index.js
+++ index.js
@@ -16,14 +16,22 @@ consentForm.addEventListener("submit", function (e) {
   modalTextEl.innerHTML = `
     <div class="modal-inner-loading">
       <img src="images/loading.svg" class="loading">
-      <p id="uploadText">
+      <p id="upload-text">
         Uploading your data to the dark web...
       </p>
     </div>
   `;
 
   setTimeout(function () {
-    const uploadText = document.getElementById("uploadText");
-    uploadText.innerHTML = `Making the sale...`;
+    document.getElementById("upload-text").innerText = `Making the sale...`;
   }, 1500);
+
+  setTimeout(function () {
+    document.getElementById("modal-inner").innerHTML = `<h2>Thanks you sucker! </h2>
+    <p>We just sold the rights to your eternal soul.</p>
+    <div class="idiot-gif">
+        <img src="images/pirate.gif">
+    </div>
+    `;
+  }, 3000);
 });
