6b6db7596b4fa5d19082e64974a3d5bfbb9e4b7d
Mohamed Ibrahim <109217422+memo-ibrahim-alean@users.noreply.github.com>
2022-11-21 13:53:41 +0200

    Add modal messge

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const modalEl = document.getElementById("modal");
 const closeBtnEl = document.getElementById("modal-close-btn");
 const consentForm = document.getElementById("login-form");
+const modalTextEl = document.getElementById("modal-text");
 
 setTimeout(function () {
   modalEl.style.display = "inline";
@@ -12,5 +13,12 @@ closeBtnEl.addEventListener("click", function () {
 
 consentForm.addEventListener("submit", function (e) {
   e.preventDefault();
-  console.log("form submitted");
+  modalTextEl.innerHTML = `
+    <div class="modal-inner-loading">
+      <img src="images/loading.svg" class="loading">
+      <p id="uploadText">
+        Uploading your data to the dark web...
+      </p>
+    </div>
+  `;
 });
