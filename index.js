d4b29a393639404a52f226d584e246421b575608
Mohamed Ibrahim <109217422+memo-ibrahim-alean@users.noreply.github.com>
2022-11-21 09:45:48 +0200

    Adding closing btn

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const modalEl = document.getElementById("modal");
+const closeBtnEl = document.getElementById("modal-close-btn");
 
 setTimeout(function () {
-  modalEl.style.display = 'inline';
+  modalEl.style.display = "inline";
 }, 1500);
+
+closeBtnEl.addEventListener("click", function () {
+  modalEl.style.display = "none";
+});
